801f8bccea987f09ebf434398c3758dbe6fa00d7
Helmoz <helmo.designer1@gmail.com>
2020-09-13 19:09:11 +0300

    Добавлено решение

---
+++ NthToLastElement/NthToLastElement/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NthToLastElement
+{
+    internal class Program
+    {
+        private static LinkedListNode<int> NthToLast(LinkedListNode<int> node, int n) {
+            var currentNode = node;
+            var follower = node;
+        
+            for (var i = 0; i < n; i++) {
+                if (currentNode == null)
+                {
+                    return null;
+                }
+                    
+                currentNode = currentNode.Next;
+            }
+        
+            if (currentNode == null) return null;
+        
+            while (currentNode.Next != null) {
+                currentNode = currentNode.Next;
+                follower = follower?.Next;
+            }
+      
+            return follower;
+        }
+        
+        static void Main(string[] args)
+        {
+            var list = new LinkedList<int>(new[] { 1, 2, 3, 4 });
+            Console.WriteLine(NthToLast(list.First, 0).Value);
+            Console.WriteLine(NthToLast(list.First, 1).Value);
+            Console.WriteLine(NthToLast(list.First, 2).Value);
+            Console.WriteLine(NthToLast(list.First, 3).Value);
+        }
+    }
+}
